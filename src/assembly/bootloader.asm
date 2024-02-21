e771671e228a008f191d92ee1d0ae36ffd103c20
Kap Petrov <148530033+KapPetrov@users.noreply.github.com>
2024-02-21 13:08:49 +0530

    Alphabet with alternating caps, and a tutorial to follow

--- src/assembly/bootloader.asm
+++ src/assembly/bootloader.asm
@@ -1,5 +1,18 @@
 
-jmp $
+mov ah, 0x0e
+mov al, 65
+int 0x10
+
+loop:
+    inc al
+    xor al, 32
+    cmp al, 'Z' + 1
+    je exit
+    int 0x10
+    jmp loop
+
+exit:
+    jmp $
 
 times 510-($-$$) db 0
 dw 0xaa55
